1dcd8cf62a24f5080e7c8c7dfd19d44148e2f991
Jordan Hudgens <jordan@devcamp.com>
2017-06-29 17:18:55 +0000

    Implemented authorization engine for portfolio items and blogs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
 def login_helper
-    if current_user.is_a?(User) 
-     link_to "Logout", destroy_user_session_path, method: :delete 
-    else 
-     (link_to "Register", new_user_registration_path) +
+    if current_user.is_a?(GuestUser) 
+       (link_to "Register", new_user_registration_path) +
      "<br>".html_safe +
      (link_to "Login", new_user_session_path )
+    else 
+    link_to "Logout", destroy_user_session_path, method: :delete 
     end 
 end
 
@@ -21,3 +21,10 @@ end
  end
 end
 
+module UnzViewTool
+  class Renderer
+    def self.copyright name, msg
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end
+end
